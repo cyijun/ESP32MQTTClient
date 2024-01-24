97556b44eb2a33c568148af8a0dd3fb4da3f2a51
VietDzung <dzungpv@outlook.com>
2024-01-24 13:44:51 +0700

    Fix crash with empty payload

--- src/ESP32MQTTClient.cpp
+++ src/ESP32MQTTClient.cpp
@@ -364,8 +364,14 @@ void ESP32MQTTClient::onMessageReceivedCallback(const char *topic, char *payload
         strTerminationPos = length;
 
     // Second, we add the string termination code at the end of the payload and we convert it to a String object
-    payload[strTerminationPos] = '\0';
+    String payloadStr;
     String payloadStr(payload);
+    if (payload) {
+      payload[strTerminationPos] = '\0';
+      payloadStr=String(payload);
+    } else {
+        payloadStr="";
+    }
     String topicStr(topic);
 
     // Logging
